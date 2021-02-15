b1e131be117f6c83f42af7b79e755efabbec0752
rokhaya <rokhydaaba63@gmail.com>
2021-02-15 16:26:06 +0000

    modification db

--- application/config/database.php
+++ application/config/database.php
@@ -74,4 +74,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = 'mysql://doadmin:tmicrtsogxbz19yu@db-mysql-fra1-08748-do-user-7459181-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED';
+$db['default'] = array(
+  'dsn' => '',
+  'hostname' => 'db-mysql-fra1-08748-do-user-7459181-0.b.db.ondigitalocean.com',
+  'username' => 'doadmin',
+  'password' => 'tmicrtsogxbz19yu',
+  'database' => 'defaultdb',
+  'dbdriver' => 'mysqli',
+  'dbprefix' => '',
+  'pconnect' => TRUE,
+  'db_debug' => TRUE,
+  'cache_on' => FALSE,
+  'cachedir' => '',
+  'char_set' => 'utf8',
+  'dbcollat' => 'utf8_general_ci',
+  'swap_pre' => '',
+  'encrypt' => FALSE,
+  'compress' => FALSE,
+  'stricton' => FALSE,
+  'failover' => array(),
+  'save_queries' => TRUE,
+  'port' => 25060,
+  'sslmode' => REQUIRED
+);
